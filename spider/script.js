db26e0c5ea0958ea1d977f1b8299e84c481a2a21
Xen <ugcbyxenboheme@gmail.com>
2025-10-14 15:37:18 -0700

    Update script.js

--- spider/script.js
+++ spider/script.js
@@ -1,1 +1,15 @@
 
+const loadingPage = document.querySelector('.loading-page');
+const dynamicContent = document.querySelector('.dynamic-content');
+const orderBtn = document.querySelector('.order-btn');
+
+loadingPage.style.display = 'block';
+
+setTimeout(() => {
+    loadingPage.style.display = 'none';
+    dynamicContent.style.display = 'block';
+}, 2000);
+
+orderBtn.addEventListener('click', () => {
+    alert('Merci pour votre commande!');
+});
